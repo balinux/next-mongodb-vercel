69952f9662edfc1c1c800f78e16a3a2ac5a501c4
balinux <ryojuniyantara@gmail.com>
2021-03-04 06:53:49 +0800

    update: add statu environt,et

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     env: {
-        MONGO_URL: "mongodb+srv://bloguser:blogpassword123@yhotiecluster0.ez522.mongodb.net/blogtutorial?retryWrites=true&w=majority"
+        MONGO_URL: "mongodb+srv://bloguser:blogpassword123@yhotiecluster0.ez522.mongodb.net/blogtutorial?retryWrites=true&w=majority",
+        url: process.env.STATUS == "PRODUCTION" ? process.env.BACKEND_PRODUCTION : process.env.BACKEND_LOCAL
     }
 }
