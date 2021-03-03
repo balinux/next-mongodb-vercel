2587bde20a650d7af50ef719b35e4a06d94aadee
balinux <ryojuniyantara@gmail.com>
2021-03-03 11:42:31 +0800

    update ; provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 // import '../styles/globals.css'
 import '../styles/custom-theme.scss'
+import { CounterProvider } from '../components/Counter'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CounterProvider>
+      <Component {...pageProps} />
+    </CounterProvider>
+  )
 }
 
 export default MyApp
