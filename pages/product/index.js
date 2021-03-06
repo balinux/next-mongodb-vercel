d2e9cf7f96cbb96096afcb1bb583dbe54e2a720a
balinux <ryojuniyantara@gmail.com>
2021-03-06 17:29:51 +0800

    update: remove index product

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,27 +1,20 @@
 export default function Product({ products }) {
     return (
         <div>
-            <h1>products</h1>
-            {
-                products.data.map(product => (
-                    <div key={products._id}>
-                        <p>{product.title}</p>
-                    </div>
-                ))
-            }
+            <h1>Nothing happens here</h1>
         </div>
     )
 }
 
 
-export async function getStaticProps() {
-    const response = await fetch("http://localhost:3000/api/product");
-    const products = await response.json();
-    console.log(products);
+// export async function getStaticProps() {
+//     const response = await fetch("http://localhost:3000/api/product");
+//     const products = await response.json();
+//     console.log(products);
 
-    return {
-        props: {
-            products
-        }
-    }
-}
+//     return {
+//         props: {
+//             products
+//         }
+//     }
+// }
